aacdad2fe2d113b783f102fecda78538fccdd97e
chenliang <chenliang@zycapital.com>
2025-08-29 18:27:09 +0800

    finish open long/short q table init

--- test.py
+++ test.py
@@ -43,11 +43,41 @@ def init_close_short_q_table():
 
 
 def init_open_long_q_table():
-    pass
+    action_list = [0, 1, 2, 3]
+    state_table_index_list = [i for i in range(6 * 6 * 6)]
+    q_table = pd.DataFrame(index=state_table_index_list, columns=action_list)
+    q_table.fillna(0.0, inplace=True)
+    index = 0
+    for delta1 in [0, 0.2, 0.4, 0.6, 0.8, 1]:
+        for delta2 in [0, 0.2, 0.4, 0.6, 0.8, 1]:
+            for delta3 in [0, 0.2, 0.4, 0.6, 0.8, 1]:
+                if delta2 > 0:
+                    q_table.at[index, 2] = 1.0
+                if delta2 == 0 and delta3 > 0:
+                    q_table.at[index, 3] = 1.0
+                index += 1
+    q_table.index.name = "state_index"
+    q_table.columns.name = "action"
+    return q_table
 
 
 def init_open_short_q_table():
-    pass
+    action_list = [0, 1, 2, 3]
+    state_table_index_list = [i for i in range(6 * 6 * 6)]
+    q_table = pd.DataFrame(index=state_table_index_list, columns=action_list)
+    q_table.fillna(0.0, inplace=True)
+    index = 0
+    for delta1 in [0, 0.2, 0.4, 0.6, 0.8, 1]:
+        for delta2 in [0, 0.2, 0.4, 0.6, 0.8, 1]:
+            for delta3 in [0, 0.2, 0.4, 0.6, 0.8, 1]:
+                if delta2 > 0:
+                    q_table.at[index, 2] = 1.0
+                if delta2 == 0 and delta3 > 0:
+                    q_table.at[index, 3] = 1.0
+                index += 1
+    q_table.index.name = "state_index"
+    q_table.columns.name = "action"
+    return q_table
 
 
 def test_mc():
